8252384fc5c4e466c1859a1283de332c44d00432
Graeme McNish <graememcnish@gmail.com>
2023-02-08 11:30:17 -0800

    correct but listed as errors on test

--- src/index.js
+++ src/index.js
@@ -39,66 +39,106 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+// Images 
 
-const footAyyy = document.querySelector("footer");
-footAyyy.textContent = siteContent["footer"]["copyright"]
-footAyyy.className = "bold";
+const logoImg = document.querySelector("#logo-img");
+logoImg.src = siteContent["images"]["logo-img"];
 
-const navI1 = document.querySelector("nav a");
-const navI2 = navI1.nextElementSibling;
-const navI3 = navI2.nextElementSibling;
-const navI4 = navI3.nextElementSibling;
-const navI5 = navI4.nextElementSibling;
-const navI6 = navI5.nextElementSibling;
-navI1.textContent = siteContent["nav"]["nav-item-1"]
-navI2.textContent = siteContent["nav"]["nav-item-2"]
-navI3.textContent = siteContent["nav"]["nav-item-3"]
-navI4.textContent = siteContent["nav"]["nav-item-4"]
-navI5.textContent = siteContent["nav"]["nav-item-5"]
-navI6.textContent = siteContent["nav"]["nav-item-6"]
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.src = siteContent["images"]["cta-img"];
 
-const logoImg = document.querySelector("#logo-img")
-const ctaImg = document.querySelector("#cta-img")
-const accentImg = document.querySelector("#middle-img")
-logoImg.src = siteContent["images"]["logo-img"]
-ctaImg.src = siteContent["images"]["cta-img"]
-accentImg.src = siteContent["images"]["accent-img"]
+const accentImg = document.querySelector("#middle-img");
+accentImg.src = siteContent["images"]["accent-img"];
 
+// Footer Link 
+
+const footer = document.querySelector("footer");
+footer.textContent = siteContent.footer.copyright;
+footer.classList.add('bold');
+
+// Contact
+const contact = document.querySelector("section.contact");
+contact.children[0].textContent = siteContent.contact["contact-h4"];
+contact.children[1].textContent = siteContent.contact.address;
+contact.children[2].textContent = siteContent.contact.phone;
+contact.children[3].textContent = siteContent.contact.email;
+
+// // my first attempt - this works but is messy
+// const contactTitle = document.querySelector(".contact h4");
+// const contactAddress = document.querySelector(".contact p");
+// const contactPhone = contactAddress.nextElementSibling;
+// const contactEmail = contactPhone.nextElementSibling;
+// contactTitle.textContent = siteContent["contact"]["contact-h4"]
+// contactPhone.textContent = siteContent["contact"]["phone"]
+// contactAddress.textContent = siteContent["contact"]["address"]
+// contactEmail.textContent = siteContent["contact"]["email"]
+
+// Top Content 
+
+const topContent = document.querySelector('.top-content');
+topContent.children[0].children[0].textContent = siteContent["main-content"]["features-h4"];
+topContent.children[0].children[1].textContent = siteContent["main-content"]["features-content"];
+topContent.children[1].children[0].textContent = siteContent["main-content"]["about-h4"];
+topContent.children[1].children[1].textContent = siteContent["main-content"]["about-content"];
+
+// Bottom Content
+
+const bottomContent = document.querySelector(".bottom-content");
+const h4sBottom = bottomContent.querySelectorAll('h4');
+h4sBottom[0].textContent = siteContent["main-content"]["services-h4"];
+h4sBottom[1].textContent = siteContent["main-content"]["product-h4"];
+h4sBottom[2].textContent = siteContent["main-content"]["vision-h4"];
+const paragBottom = bottomContent.querySelectorAll('p');
+paragBottom[0].textContent = siteContent["main-content"]["services-content"]
+paragBottom[1].textContent = siteContent["main-content"]["product-content"]
+paragBottom[2].textContent = siteContent["main-content"]["vision-content"]
+
+// // my attempt - works but is messy 
+// const mainNum1 = document.querySelector('h4')
+// const mainNum2 = mainNum1.nextElementSibling;
+
+// mainNum1.textContent = siteContent["main-content"]["features-h4"]
+// mainNum2.textContent = siteContent["main-content"]["features-content"]
+
+// const allH4 = document.querySelectorAll('h4')
+// const allParag = document.querySelectorAll('.text-content p')
+
+// allH4[1].textContent = siteContent["main-content"]["about-h4"]
+// allParag[1].textContent = siteContent["main-content"]["about-content"]
+// allH4[2].textContent = siteContent["main-content"]["services-h4"]
+// allParag[2].textContent = siteContent["main-content"]["services-content"]
+// allH4[3].textContent = siteContent["main-content"]["product-h4"]
+// allParag[3].textContent = siteContent["main-content"]["product-content"]
+// allH4[4].textContent = siteContent["main-content"]["vision-h4"]
+// allParag[4].textContent = siteContent["main-content"]["vision-content"]
+
+// CTA 
 const titleCta = document.querySelector("h1")
 const button = document.querySelector("button")
 titleCta.textContent = siteContent["cta"]["h1"]
 button.textContent = siteContent["cta"]["button"]
 
-const mainNum1 = document.querySelector('h4')
-const mainNum2 = mainNum1.nextElementSibling;
+// NAV Links 
 
-console.log(mainNum1)
-mainNum1.textContent = siteContent["main-content"]["features-h4"]
-mainNum2.textContent = siteContent["main-content"]["features-content"]
-
-const allH4 = document.querySelectorAll('h4')
-const allParag = document.querySelectorAll('.text-content p')
-
-console.log(allParag)
-allH4[1].textContent = siteContent["main-content"]["about-h4"]
-allParag[1].textContent = siteContent["main-content"]["about-content"]
-allH4[2].textContent = siteContent["main-content"]["services-h4"]
-allParag[2].textContent = siteContent["main-content"]["services-content"]
-allH4[3].textContent = siteContent["main-content"]["product-h4"]
-allParag[3].textContent = siteContent["main-content"]["product-content"]
-allH4[4].textContent = siteContent["main-content"]["vision-h4"]
-allParag[4].textContent = siteContent["main-content"]["vision-content"]
-
-const contactTitle = document.querySelector(".contact h4");
-const contactAddress = document.querySelector(".contact p");
-const contactPhone = contactAddress.nextElementSibling;
-const contactEmail = contactPhone.nextElementSibling;
-contactTitle.textContent = siteContent["contact"]["contact-h4"]
-contactPhone.textContent = siteContent["contact"]["phone"]
-contactAddress.textContent = siteContent["contact"]["address"]
-contactEmail.textContent = siteContent["contact"]["email"]
+// I am commenting out my original work in order to write 'DRY' code
+// const navI1 = document.querySelector("nav a");
+// const navI2 = navI1.nextElementSibling;
+// const navI3 = navI2.nextElementSibling;
+// const navI4 = navI3.nextElementSibling;
+// const navI5 = navI4.nextElementSibling;
+// const navI6 = navI5.nextElementSibling;
+// navI1.textContent = siteContent["nav"]["nav-item-1"]
+// navI2.textContent = siteContent["nav"]["nav-item-2"]
+// navI3.textContent = siteContent["nav"]["nav-item-3"]
+// navI4.textContent = siteContent["nav"]["nav-item-4"]
+// navI5.textContent = siteContent["nav"]["nav-item-5"]
+// navI6.textContent = siteContent["nav"]["nav-item-6"]
 
 const allNav = document.querySelectorAll('nav a');
-allNav.forEach(element => element.className = 'italic');
+const navLinkTexts = Object.values(siteContent.nav);
+// allNav.forEach(element => element.className = 'italic');
+allNav.forEach((link, idx) => {
+  link.textContent = navLinkTexts[idx]
+  link.classList.add('italic')
+}); 
 
